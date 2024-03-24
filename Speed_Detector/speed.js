5238d392fd0754b1162f639f864b9b336df8bf75
abdulbarik <abdulbariky000@gmail.com>
2024-03-24 21:42:38 +0300

    function to check the speed

---
+++ Speed_Detector/speed.js
@@ -0,0 +1,15 @@
+function checkSpeed() {
+    let speed = document.getElementById("speed").value;
+    let demerit_points = 0;
+    if (speed < 70) {        
+        document.getElementById("result").innerHTML = "Ok";
+    }
+     else {
+        demerit_points = Math.floor((speed - 70) / 5);
+        if (demerit_points > 12) {
+            document.getElementById("result").innerHTML = "Licence suspended";
+        } else {
+            document.getElementById("result").innerHTML = "Points: " + demerit_points;
+        }
+    }
+}
