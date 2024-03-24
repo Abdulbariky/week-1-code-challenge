3f864e09e314fccb0cd0f63c37180bfcb460a954
abdulbarik <abdulbariky000@gmail.com>
2024-03-24 21:17:30 +0300

    getting the html elements then converting the string to number

---
+++ Salary_Calculator/salary.js
@@ -0,0 +1,13 @@
+// Get HTML elements
+let benefits = document.querySelector("#benefits").value
+const button = document.querySelector(".button")
+// Convert string to number
+benefits = parseFloat(benefits)
+// initialise Variables
+let nhif;
+let nssf;
+let insuranceRelief;
+let personalRelief;
+let payee;
+let taxablePay;
+let netPay;
