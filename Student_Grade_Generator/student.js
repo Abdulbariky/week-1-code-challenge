13041868f373bf9824e4f20ad5310437970f5dc5
abdulbarik <abdulbariky000@gmail.com>
2024-03-24 22:42:07 +0300

    checking comments

--- Student_Grade_Generator/student.js
+++ Student_Grade_Generator/student.js
@@ -12,17 +12,17 @@ function check() {
     let sum = parseFloat(php) + parseFloat(java) + parseFloat(flutter) + parseFloat(laravel);
     let average = (sum / 400) * 100;
 
-     // Check if any of the input values are empty or out of range
+     // Check  the input values if empty or out of range
      if (php === '' || java === '' || flutter === '' || laravel === '' ||
      php < 0 || php > 100 || java < 0 || java > 100 || flutter < 0 || flutter > 100 ||
      laravel < 0 || laravel > 100) {
-     // If it is, display a message in the result element
+     //  displaying a message in the result element
      grade.innerHTML = `<p>Please Enter A Valid Value Between 0 and 100</p>`
      totalMarks.innerHTML = ``
      percentage.innerHTML = ``
 
  } 
-  // checks for other condition
+  // checks otherwise
   else {
     if (average > 79 && average <= 100) {
         grade.innerHTML = `Grade: A`
@@ -41,5 +41,5 @@ function check() {
 
 }
 
-// Adds event listener to the button
+// Adding eventlistener to the button
 button.addEventListener("click", check)
