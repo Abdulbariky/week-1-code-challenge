d552b53d7570556aa7a9644a96e50fb3b23d21b4
abdulbarik <abdulbariky000@gmail.com>
2024-03-24 22:47:00 +0300

    comments

--- Speed_Detector/speed.js
+++ Speed_Detector/speed.js
@@ -1,12 +1,14 @@
 function checkSpeed() {
     let speed = document.getElementById("speed").value;
     let demerit_points = 0;
+    // cheking the conditions to print ok if speed is less than 70 // 
     if (speed < 70) {        
         document.getElementById("result").innerHTML = "Ok";
     }
      else {
         demerit_points = Math.floor((speed - 70) / 5);
         if (demerit_points > 12) {
+            // printing licence suspended if more than 12 demerits 
             document.getElementById("result").innerHTML = "Licence suspended";
         } else {
             document.getElementById("result").innerHTML = "Points: " + demerit_points;
