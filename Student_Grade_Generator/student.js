f786f2fd6b84f984d96f4bd4ba56797894773f87
abdulbarik <abdulbariky000@gmail.com>
2024-03-24 22:04:58 +0300

    checking if any of the input values is out of range

--- Student_Grade_Generator/student.js
+++ Student_Grade_Generator/student.js
@@ -12,9 +12,25 @@ function check() {
     let sum = parseFloat(php) + parseFloat(java) + parseFloat(flutter) + parseFloat(laravel);
     let average = (sum / 400) * 100;
 
+     // Check if any of the input values are empty or out of range
+     if (php === '' || java === '' || flutter === '' || laravel === '' ||
+     php < 0 || php > 100 || java < 0 || java > 100 || flutter < 0 || flutter > 100 ||
+     laravel < 0 || laravel > 100) {
+     // If it is, display a message in the result element
+     grade.innerHTML = `<p>Please Enter A Valid Value Between 0 and 100</p>`
+     totalMarks.innerHTML = ``
+     percentage.innerHTML = ``
+
+ } 
 
 
 
 
+
+
+
+
+
+ 
 
 }
